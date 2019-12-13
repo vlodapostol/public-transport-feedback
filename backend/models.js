6e6ea62a81e7ebb9e6b651233f9b99166cd26b62
Ubuntu <ubuntu@ip-172-31-16-51.us-east-2.compute.internal>
2019-12-13 16:46:05 +0000

    initial commit

---
+++ backend/models.js
@@ -0,0 +1,88 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('project_DB', 'root', 'p@ss', {
+    dialect: 'mysql'
+});
+
+sequelize.authenticate().then(() => {
+    console.log('connection succesful');
+}).catch((err) => {
+    console.log(`error on connection:${err}`);
+});
+
+class User extends Sequelize.Model {};
+User.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {sequelize, modelName:'user'});
+
+class Feedback extends Sequelize.Model {};
+Feedback.init({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    startingPoint: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    destinationPoint: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    transportType: {
+        type: Sequelize.STRING,
+        validate: {
+            validateType(value) {
+                if (!(value === 'BUS' || value === 'METRO' || value === 'TRAM')) {
+                    throw new Error('Transport type can only be: BUS, METRO or TRAM');
+                }
+            }
+        }
+
+    },
+    departureHour: {
+        type: Sequelize.TIME,
+        allowNull: false
+    },
+    tripDuration: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    crowdednessLevel: {
+        type: Sequelize.STRING,
+        validate: {
+            validateType(value) {
+                if (!(value === 'LOW' || value === 'MEDIUM' || value === 'HIGH')) {
+                    throw new Error('Transport type can only be: LOW, MEDIUM or HIGH');
+                }
+            }
+        }
+    },
+    observations: {
+        type: Sequelize.STRING
+    },
+    satisfactionLevel: {
+        type: Sequelize.INTEGER
+    }
+}, { sequelize, modelName: 'feedback' });
+
+sequelize.sync();
+
+module.exports = { sequelize, Feedback };
